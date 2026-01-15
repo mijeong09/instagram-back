f5f017bdeb84bb933208d62463c8995392c1fb05
mijeong09 <me9809180@gmail.com>
2026-01-15 14:49:09 +0900

    update action

--- src/main/java/com/example/instagramapi/config/SecurityConfig.java
+++ src/main/java/com/example/instagramapi/config/SecurityConfig.java
@@ -53,7 +53,8 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        // configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOriginPatterns(List.of("*"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
